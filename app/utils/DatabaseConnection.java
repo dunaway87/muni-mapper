1bb91dddb9c8a92f1d490565bb829450a4913fa3
dunaway87 <johnmarc@axiomalaska.com>
2014-03-16 00:36:22 -0800

    hehe

--- app/utils/DatabaseConnection.java
+++ app/utils/DatabaseConnection.java
@@ -8,9 +8,9 @@ public class DatabaseConnection {
 	
 	public static Connection getConnection() throws SQLException{
 	//	Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:63334/automated_shapefile_uploads?user=dunawayjenckes&password=MacsEdgar2155");
-		Connection conn = DriverManager.getConnection("jdbc:postgresql://geoblaster.info:5432/automated_shapefile_uploads?user=dunawayjenckes&password=MacsEdgar2155");
+	//	Connection conn = DriverManager.getConnection("jdbc:postgresql://geoblaster.info:5432/automated_shapefile_uploads?user=dunawayjenckes&password=MacsEdgar2155");
 
-	//	Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/automated_shapefile_uploads?user=dunawayjenckes&password=MacsEdgar2155");
+		Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/automated_shapefile_uploads?user=dunawayjenckes&password=dunawayjenckes");
 		return conn;
 	}
 }
